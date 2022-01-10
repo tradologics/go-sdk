aa8a4b05f2a50b3e19907a0ca6a6ae8cb0526f8a
vitalii honchar <honchar.vitalii@gmail.com>
2022-01-10 15:28:29 +0200

    Upgrade strategy handler, exclude http request/response logic from customer strategy.

--- server/server.go
+++ server/server.go
@@ -1,17 +1,45 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-var strategyHandler func(w http.ResponseWriter, r *http.Request)
+var strategyHandler func(tradehook string, payload []byte)
+
+// strategyWrapper retrieve tradehook information from response body and send it to customer strategy
+func strategyWrapper(w http.ResponseWriter, r *http.Request) {
+	var requestBody struct {
+		Tradehook string `json:"tradehook"`
+		Payload   []byte `json:"payload"`
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, &requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	strategyHandler(requestBody.Tradehook, requestBody.Payload)
+
+	w.WriteHeader(200)
+	_, err = w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 // postMethodOnlyHandler validate request method and execute only 'POST'
 func postMethodOnlyHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		strategyHandler(w, r)
+		strategyWrapper(w, r)
 	} else {
 		w.WriteHeader(405)
 		_, err := w.Write([]byte(http.StatusText(http.StatusMethodNotAllowed)))
@@ -22,7 +50,7 @@ func postMethodOnlyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // router returns http server mux with selected handler and URL path
-func router(strategy func(w http.ResponseWriter, r *http.Request), endpoint string) http.Handler {
+func router(strategy func(tradehook string, payload []byte), endpoint string) http.Handler {
 	strategyHandler = strategy
 
 	router := http.NewServeMux()
@@ -32,7 +60,8 @@ func router(strategy func(w http.ResponseWriter, r *http.Request), endpoint stri
 }
 
 // Start create new server with selected host and port, and use strategy as request handler
-func Start(strategy func(w http.ResponseWriter, r *http.Request), endpoint, host string, port int) {
+func Start(strategy func(tradehook string, payload []byte), endpoint, host string, port int) {
+
 	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router(strategy, endpoint))
 	if err != nil {
 		log.Fatalln(err)
--- server/server_test.go
+++ server/server_test.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,13 +21,19 @@ func cls(b io.ReadCloser) {
 func TestRunServerWithStrategy(t *testing.T) {
 
 	// Create handler
-	strategy := func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
+	strategy := func(tradehook string, payload []byte) {
+		var payloadData struct {
+			Assets []string    `json:"assets"`
+			Bars   interface{} `json:"bars"`
+		}
 
-		_, err := w.Write([]byte("ok"))
+		err := json.Unmarshal(payload, &payloadData)
 		if err != nil {
 			assert.NoError(t, err)
 		}
+
+		assert.Equal(t, "bars", tradehook, invalidErrorMsg)
+		assert.NotEqualf(t, 0, len(payloadData.Assets), invalidErrorMsg)
 	}
 
 	// Start server
@@ -43,13 +52,21 @@ func TestRunServerWithStrategy(t *testing.T) {
 	cls(res.Body)
 
 	// Post request
-	res, err = http.Post(fmt.Sprintf("%s/", server.URL), "", nil)
+	data, err := json.Marshal(struct {
+		Tradehook string
+		Payload   []byte
+	}{
+		Tradehook: "bars",
+		Payload:   []byte("{\n  \"assets\": [\"AAPL:US\", \"BTCUSD:BMEX\", \"...\"],\n  \"bars\": {\n    \"YYYY-MM-01\": {\n      \"AAPL\": {\n        \"o\": \"113.79\",\n        \"h\": \"117.26\",\n        \"l\": \"113.62\",\n        \"c\": \"115.56\",\n        \"v\": 136210200,\n        \"t\": 782692,\n        \"w\": \"116.11665173913042\"\n      },\n      \"BTCUSD:BMEX\": {\n        \"o\": \"...\",\n        \"h\": \"...\",\n        \"l\": \"...\",\n        \"c\": \"...\",\n        \"v\": \"...\",\n        \"t\": \"...\",\n        \"w\": \"...\"\n      }\n    }\n  }\n}"),
+	})
+
+	res, err = http.Post(fmt.Sprintf("%s/", server.URL), "", ioutil.NopCloser(bytes.NewBuffer(data)))
 	if err != nil {
 		assert.NoError(t, err)
 	}
 
 	body, err = io.ReadAll(res.Body)
-	assert.Equal(t, "ok", string(body), invalidErrorMsg)
+	assert.Equal(t, "OK", string(body), invalidErrorMsg)
 	assert.Equal(t, 200, res.StatusCode, invalidErrorMsg)
 	cls(res.Body)
 }
